eb00fec3d396f1b6557f5c559042bcb50b63cc63
Martin-Cifre <martin.cifre@outlook.com>
2024-02-04 19:22:40 -0300

    estructura header

---
+++ src/components/Headder/Headder.tsx
@@ -0,0 +1,36 @@
+import { getImgUrl } from "../../utils.js";
+import "./Headder.module.css";
+import styles from "./Headder.module.css";
+
+
+export function Headder() {
+  return (
+    <header>
+      <div>
+        <div>
+          <img
+            className={styles.bgHeader}
+            src={getImgUrl("headder/blur.jpeg")}
+            alt="imagen del hotel borrosa"
+          />
+        </div>
+
+        <div className={styles.header}>
+          <div className={styles.logo}>
+            <p className={styles.logoHotel}>Hotel</p>
+            <p className={styles.logoParaiso}>Paraiso</p>
+          </div>
+          <div className={styles.links}>
+            <ul>
+              <li>Quienes Somos</li>
+              <li>Habitaciones</li>
+              <li>Contacto</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Headder;
