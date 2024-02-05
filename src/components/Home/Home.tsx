fdd916714c080b8d054c5b33640859e066e6f55a
Martin-Cifre <martin.cifre@outlook.com>
2024-02-05 17:06:53 -0300

    agregados archivos para home y variables de colores y textos

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+//import { getImgUrl } from "../../utils.js";
+import "./Home.module.css";
+//import styles from "./Home.module.css";
+
+export function Home() {
+  return (
+    <main>
+      <p>pepito bonito</p>
+    </main>
+  );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Headder from './components/Headder/Headder.tsx'
+import Home from './components/Home/Home.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Headder />
+    <Home />
   </React.StrictMode>,
 )
