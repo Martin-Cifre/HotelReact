c778631cfce86ab8c6f0ffbd6d6f6b34af6c5254
Martin-Cifre <martin.cifre@outlook.com>
2024-02-06 19:49:12 -0300

    comenzando seccion about

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Headder from './components/Headder/Headder.tsx'
-import Home from './components/Home/Home.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Headder from './components/Headder/Headder.tsx';
+import Home from './components/Home/Home.tsx';
+import { About } from './components/About/About.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Headder />
     <Home />
+    <About />
   </React.StrictMode>,
 )
