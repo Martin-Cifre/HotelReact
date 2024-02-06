8e695e3cbf242547c68b591d28fec8ea226a3cbe
Martin-Cifre <martin.cifre@outlook.com>
2024-02-06 20:55:59 -0300

    armada la estructura y agregada la informacion de la seccion about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,33 @@
 //import React from 'react';
+import { getImgUrl } from "../../utils.js";
+import styles from "./About.module.css";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
+    <section>
+      <div>
+        <img
+          className={styles.img}
+          src={getImgUrl("About/About.png")}
+          alt="About Hotel Paraiso"
+        />
+        <div>
+          <h2>Hotel Paraiso</h2>
+          <p>
+            Ubicado en una isla paradisiaca de ensueño, el Hotel Paraíso emerge
+            como un oasis de fantasía para los viajeros exigentes. Con una
+            apertura prevista para el 2023, nuestras instalaciones representarán
+            la máxima expresión del lujo y la comodidad. Ofreceremos una
+            experiencia única e incomparable a precios que desafiarán cualquier
+            estándar de la competencia en la región. Estratégicamente ubicado a
+            solo 20 minutos en coche del Aeropuerto Internacional Lynden
+            Pindling y a 15 minutos de los centros turísticos más vibrantes de
+            Nassau. Sumérjase en la exuberancia tropical de nuestras
+            instalaciones mientras exploramos juntos los misterios y maravillas
+            de este paraíso terrenal.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
