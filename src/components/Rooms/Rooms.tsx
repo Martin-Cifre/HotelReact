93c36cb2828cfdad7361f23fce10239d5554ec40
Martin-Cifre <martin.cifre@outlook.com>
2024-02-07 20:10:04 -0300

    estructura Rooms

---
+++ src/components/Rooms/Rooms.tsx
@@ -0,0 +1,8 @@
+//import React from 'react'
+import styles from "./Rooms.module.css";
+
+export const Rooms = () => {
+  return (
+    <div className={styles.tata}>Rooms</div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import Headder from './components/Headder/Headder.tsx';
 import Home from './components/Home/Home.tsx';
 import { About } from './components/About/About.tsx';
 import { Location } from './components/Location/Location.tsx';
+import { Rooms } from './components/Rooms/Rooms.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Home />
     <About />
     <Location />
+    <Rooms />
   </React.StrictMode>,
 )
