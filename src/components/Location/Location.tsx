b9b974bef919b99465d00e9cba6bdc3072942d97
Martin-Cifre <martin.cifre@outlook.com>
2024-02-06 21:32:35 -0300

    agregados estilos a la seccion Location

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -9,23 +9,12 @@ export const Location = () => {
         <div className={styles.containerTxt}>
           <h2>Conoce Nassau</h2>
           <p>
-            Anclado estratégicamente en el corazón histórico de Nassau, nuestro
-            hotel se erige como la puerta de entrada a las fascinantes historias
-            que esta vibrante isla tiene para contar. Embárcate en un viaje a
-            través del tiempo explorando el pasado pirata de Nassau, donde
-            sitios legendarios como el Fuerte Charlotte y el Museo Pirata de
-            Nassau te sumergirán en la intrépida vida de los corsarios del
-            Caribe. Recorre las callejuelas empedradas de la ciudad colonial,
-            donde cada adoquín lleva impreso el eco de siglos pasados. Admira la
-            majestuosa arquitectura colonial española y británica mientras
-            deambulas por los barrios históricos de Nassau, donde las
-            influencias culturales se entrelazan en un cautivador tapiz.
-            Descubre los secretos de la Revolución Americana y la importancia de
-            Nassau como refugio para los leales a la corona británica en el
-            siglo XVIII. Con nuestro hotel como tu punto de partida, te
-            invitamos a sumergirte en la rica historia y el encanto eterno de
-            Nassau, donde cada esquina es un relato y cada rincón espera ansioso
-            ser descubierto en el fascinante libro de la historia del Caribe.
+            En Nassau, nuestro hotel es la puerta a su historia pirata y
+            colonial. Descubre calles empedradas, arquitectura majestuosa y
+            secretos. Sumérgete en la rica historia de Nassau. Disfruta de la
+            vibrante vida nocturna y las playas de ensueño que esta isla
+            caribeña tiene para ofrecer, todo desde la comodidad de nuestro
+            alojamiento.
           </p>
         </div>
         <img
