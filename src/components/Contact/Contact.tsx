989aefd48d888b361513dcccbf96220bd2146ae8
Martin-Cifre <martin.cifre@outlook.com>
2024-02-08 15:33:19 -0300

    Contact estructura

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,8 @@
+//import React from 'react'
+//import { getImgUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home.tsx';
 import { About } from './components/About/About.tsx';
 import { Location } from './components/Location/Location.tsx';
 import { Rooms } from './components/Rooms/Rooms.tsx';
+import { Contact } from './components/Contact/Contact.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -14,5 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <About />
     <Location />
     <Rooms />
+    <Contact />
   </React.StrictMode>,
 )
