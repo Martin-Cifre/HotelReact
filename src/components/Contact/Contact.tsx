6c85d3b5b5075c8d263b5070e717c992eefff728
Martin-Cifre <martin.cifre@outlook.com>
2024-02-08 16:11:11 -0300

    formulario de contacto agregado

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,7 +6,21 @@ export const Contact = () => {
   return (
     <footer className={styles.container}>
       <div className={styles.imgBg}>
-        <h1 className={styles.title}>Contactanos</h1>
+        
+        <h1 className={styles.title}>Contactanos</h1>     
+
+        <form className={styles.form}>
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" id="name" name="name" />
+
+          <label htmlFor="email">Correo electrónico:</label>
+          <input type="email" id="email" name="email" />
+            <br/>
+          <label htmlFor="message">Mensaje:</label>
+          <textarea id="message" name="message"></textarea>
+
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     </footer>
   );
