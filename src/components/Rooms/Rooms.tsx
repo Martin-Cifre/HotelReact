f46574f2ff0cf2c4eb7a670f04d95dd7a644c043
Martin-Cifre <martin.cifre@outlook.com>
2024-02-07 21:10:57 -0300

    html preparado

--- src/components/Rooms/Rooms.tsx
+++ src/components/Rooms/Rooms.tsx
@@ -1,8 +1,47 @@
 //import React from 'react'
 import styles from "./Rooms.module.css";
+import { getImgUrl } from "../../utils.js";
 
 export const Rooms = () => {
   return (
-    <div className={styles.tata}>Rooms</div>
-  )
+    <section className={styles.tata}>
+      <h2>Habitaciones</h2>
+      <div>
+        <div>
+          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono WiFi" />
+          <p>Wifi Gratuito</p>
+        </div>
+
+        <div>
+          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Room Service" />
+          <p>Room Service</p>
+        </div>
+
+        <div>
+          <img
+            src={getImgUrl("Rooms/WiFi.png")}
+            alt="Icono Aire Acondicionado"
+          />
+          <p>Aire Acondicionado</p>
+        </div>
+
+        <div>
+          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono All Inclusive" />
+          <p>All Inclusive</p>
+        </div>
+
+        <div>
+          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Gimnasio" />
+          <p>Gimnasio</p>
+        </div>
+
+        <div>
+          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Shows" />
+          <p>Shows exclusivos</p>
+        </div>
+      </div>
+
+      <h4>Mira las habitaciones</h4>
+    </section>
+  );
 }
