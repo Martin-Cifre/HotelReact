240de910bc1668987eb6a913965f03a6651dd91e
Martin-Cifre <martin.cifre@outlook.com>
2024-02-08 16:55:28 -0300

    redes sociales agregadas

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,27 +1,50 @@
 //import React from 'react'
-//import { getImgUrl } from "../../utils";
+import { getImgUrl } from "../../utils";
 import styles from "./Contact.module.css";
 
 export const Contact = () => {
   return (
     <footer className={styles.container}>
       <div className={styles.imgBg}>
-        
-        <h1 className={styles.title}>Contactanos</h1>     
+        <h1 className={styles.title}>Contactanos</h1>
 
         <form className={styles.form}>
-          
-          <input type="text" id="name" name="name" placeholder="Nombre"/>
+          <input type="text" id="name" name="name" placeholder="Nombre" />
 
-          
-          <input type="email" id="email" name="email" placeholder="Correo electronico"/>
-            <br/>
-          
-          <textarea id="message" name="message" placeholder="Mensaje"></textarea>
-             <br/>
-          
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Correo electronico"
+          />
+          <br />
+
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Mensaje"
+          ></textarea>
+          <br />
+
           <button type="button">Enviar</button>
         </form>
+
+        <div className={styles.linea} />
+
+        <div className={styles.redes}>
+          <div>
+            <img src={getImgUrl("/Contact/instagram.png")} alt="" />
+            <p>@usuariohotel</p>
+          </div>
+          <div>
+            <img src={getImgUrl("/Contact/facebbok.png")} alt="" />
+            <p>@usuariohotel</p>
+          </div>
+          <div>
+            <img src={getImgUrl("/Contact/mail.png")} alt="" />
+            <p>info@usuariohotel.com</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
