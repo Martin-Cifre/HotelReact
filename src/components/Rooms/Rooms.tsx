9b12da4ec8e3a31fcbd84d49309db7760a4905cf
Martin-Cifre <martin.cifre@outlook.com>
2024-02-07 22:14:34 -0300

    css Rooms

--- src/components/Rooms/Rooms.tsx
+++ src/components/Rooms/Rooms.tsx
@@ -4,44 +4,45 @@ import { getImgUrl } from "../../utils.js";
 
 export const Rooms = () => {
   return (
-    <section className={styles.tata}>
-      <h2>Habitaciones</h2>
-      <div>
-        <div>
+    <section className={styles.container}>
+      <h2 className={styles.title}>Amenities</h2>
+      <div className={styles.items}>
+        <div className={styles.item}>
           <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono WiFi" />
           <p>Wifi Gratuito</p>
         </div>
 
-        <div>
-          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Room Service" />
+        <div className={styles.item}>
+          <img src={getImgUrl("Rooms/RoomS.png")}
+            alt="Icono Room Service" />
           <p>Room Service</p>
         </div>
 
-        <div>
+        <div className={styles.item}>
           <img
-            src={getImgUrl("Rooms/WiFi.png")}
+            src={getImgUrl("Rooms/Aire.png")}
             alt="Icono Aire Acondicionado"
           />
           <p>Aire Acondicionado</p>
         </div>
 
-        <div>
-          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono All Inclusive" />
+        <div className={styles.item}>
+          <img src={getImgUrl("Rooms/AllInc.png")} alt="Icono All Inclusive" />
           <p>All Inclusive</p>
         </div>
 
-        <div>
-          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Gimnasio" />
+        <div className={styles.item}>
+          <img src={getImgUrl("Rooms/Gym.png")} alt="Icono Gimnasio" />
           <p>Gimnasio</p>
         </div>
 
-        <div>
-          <img src={getImgUrl("Rooms/WiFi.png")} alt="Icono Shows" />
+        <div className={styles.item}>
+          <img src={getImgUrl("Rooms/Show.png")} alt="Icono Shows" />
           <p>Shows exclusivos</p>
         </div>
       </div>
 
-      <h4>Mira las habitaciones</h4>
+      <h4 className={styles.rooms}>Mira las habitaciones</h4>
     </section>
   );
 }
