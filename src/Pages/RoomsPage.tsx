d1efaa52b61cd39fa0b7f154ba616d7577d5f1c1
Martin-Cifre <martin.cifre@outlook.com>
2024-02-11 19:46:25 -0300

    estructura basica de roomSite

--- src/Pages/RoomsPage.tsx
+++ src/Pages/RoomsPage.tsx
@@ -1,6 +1,7 @@
 //import React from 'react'
 import Headder from "../components/Headder/Headder"
 import styles from "./Rooms.module.css";
+import { getImgUrl } from "../utils";
 
 export const RoomsPage = () => {
   return (
@@ -10,6 +11,23 @@ export const RoomsPage = () => {
       <section className={styles.container}>
         <h2 className={styles.title}>Nuestras Habitaciones</h2>
         <div className={styles.division}></div>
+
+        <div className={styles.roomContainer}>
+          <img
+            className={styles.roomImg}
+            src={getImgUrl("RoomSite/miniClasic.jpg")}
+            alt="imagen Mini Clasic"
+          />
+          <div className={styles.detail}>
+            <h3>Mini Clasic</h3>
+            <p className={styles.parrafo}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae recusandae facilis eum distinctio, natus consequatur nisi aliquam alias, modi atque voluptatem adipisci est. Numquam eveniet molestias ipsa veritatis reiciendis?
+            </p>
+            <p className={styles.precio}> USD 70-</p>
+            <button className={styles.button}>Reservar</button>
+          </div>
+        </div>
+
       </section>
     </main>
   );
